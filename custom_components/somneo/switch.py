972404ba7538f65e9564577106dc05736d5719e9
theneweinstein <34983911+theneweinstein@users.noreply.github.com>
2021-08-16 19:33:36 +0200

    Bug fixing

--- custom_components/somneo/switch.py
+++ custom_components/somneo/switch.py
@@ -5,7 +5,6 @@ from custom_components import somneo
 from homeassistant.const import STATE_OFF, STATE_ON
 try:
     from homeassistant.components.switch import SwitchEntity
-    from homeassistant.components.number import NumberEntity
 except ImportError:
     from homeassistant.components.switch import SwitchDevice as SwitchEntity
 
@@ -29,15 +28,12 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
 
     alarms = []
     for alarm in list(data.somneo.alarms()):
-        alarms.append(SomneoToggle(name, data, device_info, dev_info['serial'], alarm, ALARMS))
-        alarms.append(SomneoToggle(name + "_" + WORKDAYS, data, device_info,
-                                   dev_info['serial'] + WORKDAYS, alarm, WORKDAYS))
-        alarms.append(SomneoToggle(name + "_" + WEEKEND, data, device_info,
-                                   dev_info['serial'] + WEEKEND, alarm, WEEKEND))
+        alarms.append(SomneoToggle(name, data, device_info, dev_info['serial'], alarm))
+
     async_add_entities(alarms, True)
 
 class SomneoToggle(SwitchEntity):
-    def __init__(self, name, data, device_info, serial, alarm, type):
+    def __init__(self, name, data, device_info, serial, alarm):
         """Initialize the switches. """
         self._data = data
         self._name = name + "_" + alarm
@@ -45,7 +41,6 @@ class SomneoToggle(SwitchEntity):
         self._device_info = device_info
         self._serial = serial
         self._state = None
-        self._type = type
 
     @property
     def name(self):
@@ -55,12 +50,7 @@ class SomneoToggle(SwitchEntity):
     @property
     def icon(self):
         """Return the icon ref of the switches."""
-        if self._type == ALARMS:
-            return ALARMS_ICON
-        elif self._type == WEEKEND:
-            return WEEKEND_ICON
-        elif self._type == WORKDAYS:
-            return WORKDAYS_ICON
+        return ALARMS_ICON
 
     @property
     def is_on(self):
@@ -98,38 +88,17 @@ class SomneoToggle(SwitchEntity):
     async def async_update(self):
         """Get the latest data and updates the states of the switches."""
         await self._data.update()
-        if self._type == ALARMS:
-            if self._data.somneo.alarms()[self._alarm]:
-                self._state = STATE_ON
-            else:
-                self._state = STATE_OFF
-        elif self._type == WORKDAYS:
-            if self._data.somneo.is_workday(self._alarm):
-                self._state = STATE_ON
-            else:
-                self._state = STATE_OFF
-        elif self._type == WEEKEND:
-            if self._data.somneo.is_weekend(self._alarm):
-                self._state = STATE_ON
-            else:
-                self._state = STATE_OFF
+        if self._data.somneo.alarms()[self._alarm]:
+            self._state = STATE_ON
+        else:
+            self._state = STATE_OFF
 
     def turn_on(self, **kwargs):
         """Called when user Turn On the switch from UI."""
-        if self._type == ALARMS:
-            self._data.somneo.toggle_alarm(True, self._alarm)
-            self._state = STATE_ON
-        elif self._type == WORKDAYS:
-            self._data.somneo.set_workdays_alarm(True, self._alarm)
-        elif self._type == WEEKEND:
-            self._data.somneo.set_weekend_alarm(True, self._alarm)
+        self._data.somneo.toggle_alarm(True, self._alarm)
+        self._state = STATE_ON
 
     def turn_off(self, **kwargs):
         """Called when user Turn Off the switch from UI."""
-        if self._type == ALARMS:
-            self._data.somneo.toggle_alarm(False, self._alarm)
-            self._state = STATE_OFF
-        elif self._type == WORKDAYS:
-            self._data.somneo.set_workdays_alarm(False, self._alarm)
-        elif self._type == WEEKEND:
-            self._data.somneo.set_weekend_alarm(False, self._alarm)
+        self._data.somneo.toggle_alarm(False, self._alarm)
+        self._state = STATE_OFF
