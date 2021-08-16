8fd2d0a8190440ac638a649d6191de078bad7876
theneweinstein <34983911+theneweinstein@users.noreply.github.com>
2021-08-16 17:59:34 +0200

    Update

--- custom_components/somneo/select.py
+++ custom_components/somneo/select.py
@@ -90,7 +90,7 @@ class SomneoDays(SelectEntity):
         return self._attr_option
 
 
-    async def select_option(self, option: str):
+    def select_option(self, option: str):
         """Called when user adjust the option in the UI."""
 
         self._attr_option = option
