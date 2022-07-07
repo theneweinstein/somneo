1e4136b05a908b6bc1019dc16f97da1d6d780601
theneweinstein <34983911+theneweinstein@users.noreply.github.com>
2022-07-07 19:22:14 +0200

    Fix depreciated features of NumberEntity

--- custom_components/smartsleep/number.py
+++ custom_components/smartsleep/number.py
@@ -45,23 +45,23 @@ class SomneoTime(NumberEntity):
         self._alarm = alarm
         self._attr_device_info = device_info
         self._attr_unique_id = serial + type + self._alarm
-        self._attr_value = 5.0
+        self._attr_native_value = 5.0
         self._type = type
         self._alarm_date = None
         if type == HOURS:
-            self._attr_min_value = 0
-            self._attr_max_value = 23
+            self._attr_native_min_value = 0
+            self._attr_native_max_value = 23
             self._attr_icon = HOURS_ICON
         elif type == MINUTES:
-            self._attr_min_value = 0
-            self._attr_max_value = 59
+            self._attr_native_min_value = 0
+            self._attr_native_max_value = 59
             self._attr_icon = MINUTES_ICON
-        self._attr_step = 1
+        self._attr_native_step = 1
 
-    def set_value(self, value: float):
+    def set_native_value(self, value: float):
         """Called when user adjust Hours / Minutes in the UI"""
         if self._alarm_date is not None:
-            self._attr_value = value
+            self._attr_native_value = value
             if self._type == MINUTES:
                 _LOGGER.debug("Set Alarm Date " + str(self._alarm_date.hour) + ":" + str(value))
                 self._data.somneo.set_alarm(self._alarm, minute=int(value))
@@ -77,17 +77,17 @@ class SomneoTime(NumberEntity):
         attr['time'], attr['days'] = self._data.somneo.alarm_settings(self._alarm)
         self._alarm_date = datetime.strptime(attr['time'],'%H:%M:%S')
         if self._type == HOURS:
-            self._attr_value = self._alarm_date.hour
+            self._attr_native_value = self._alarm_date.hour
         elif self._type == MINUTES:
-            self._attr_value = self._alarm_date.minute
+            self._attr_native_value = self._alarm_date.minute
 
 class SomneoSnooze(NumberEntity):
     _attr_should_poll = True
     _attr_available = True
     _attr_assumed_state = False
-    _attr_min_value = 1
-    _attr_max_value = 20
-    _attr_step = 1
+    _attr_native_min_value = 1
+    _attr_native_max_value = 20
+    _attr_native_step = 1
     _attr_icon = 'hass:alarm-snooze'
 
 
@@ -96,15 +96,15 @@ class SomneoSnooze(NumberEntity):
         self._data = data
         self._attr_name = name + '_snooze_time'
         self._attr_unique_id = serial + '_snooze_time'
-        self._attr_value = 9.0
+        self._attr_native_value = 9.0
         self._attr_device_info = device_info
 
-    def set_value(self, value: float):
+    def set_native_value(self, value: float):
         """Called when user adjust snooze time in the UI"""
-        self._attr_value = value
+        self._attr_native_value = value
         self._data.somneo.set_snooze_time(int(value))
 
     async def async_update(self):
         """Get the latest data and updates the states."""
         await self._data.update()
-        self._attr_value = self._data.somneo.snoozetime
+        self._attr_native_value = self._data.somneo.snoozetime
