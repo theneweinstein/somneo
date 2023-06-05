dfbf547e7436121bcd6130771bc13f190ae6c458
Kyle Wright <kyle@fkylewright.com>
2023-06-04 23:14:50 -0700

    Revert "Merge time and init changes from upstream"

--- custom_components/smartsleep/__init__.py
+++ custom_components/smartsleep/__init__.py
@@ -1,258 +1,98 @@
-""" Support for Philips SmartSleep devices."""
-from __future__ import annotations
-
-from datetime import timedelta
-import logging
+""" Philips SmartSleep """
 import asyncio
+import datetime
+import logging
+import urllib3
+import requests
+import json
+import xml.etree.ElementTree as ET
 
+from homeassistant.helpers import discovery
+from homeassistant.util import Throttle
+from homeassistant.config_entries import SOURCE_IMPORT
+from homeassistant.exceptions import PlatformNotReady
 from pysomneo import Somneo
-
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_HOST, Platform
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.debounce import Debouncer
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
-
-from .const import DOMAIN, UNKNOWN, WEEKEND, WORKDAYS, TOMORROW, EVERYDAY, CONF_SESSION
+from .const import DOMAIN, CONF_HOST, CONF_NAME, PLATFORMS, NOTIFICATION_ID, NOTIFICATION_TITLE, UPDATE_TIME
 
 _LOGGER = logging.getLogger(__name__)
 
-PLATFORMS = [
-    Platform.LIGHT,
-    Platform.NUMBER,
-    Platform.SELECT,
-    Platform.SENSOR,
-    Platform.SWITCH,
-    Platform.TIME,
-]
-SCAN_INTERVAL = timedelta(seconds=60)
 
-
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Setup the Somneo component."""
-    host = entry.data[CONF_HOST]
-
-    coordinator = SomneoCoordinator(
-        hass, host, use_session=entry.options.get(CONF_SESSION, True)
-    )
-    entry.async_on_unload(entry.add_update_listener(update_listener))
-
-    await coordinator.async_config_entry_first_refresh()
-
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = coordinator
-
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-
-    return True
-
-
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
-
-    return unload_ok
-
-
-async def update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
-    """Handle options update."""
-    await hass.config_entries.async_reload(entry.entry_id)
-
-
-async def async_migrate_entry(hass, config_entry: ConfigEntry):
-    """Migrate old entry."""
-    _LOGGER.debug("Migrating from version %s", config_entry.version)
-
-    if config_entry.version == 1:
-        new = {**config_entry.data}
-        new.update({"options": {"use_session": True}})
-
-        config_entry.version = 3
-        hass.config_entries.async_update_entry(config_entry, data=new)
-
-    if config_entry.version == 2:
-        new = {**config_entry.data}
-        use_session = new.pop("use_session")
-        new.update({"options": {"use_session": use_session}})
-
-        config_entry.version = 3
-        hass.config_entries.async_update_entry(config_entry, data=new)
-
-    _LOGGER.info("Migration to version %s successful", config_entry.version)
-
-    return True
-
-
-class SomneoCoordinator(DataUpdateCoordinator[None]):
-    """Representation of a SmartSleep Coordinator."""
-
-    def __init__(
-        self,
-        hass: HomeAssistant,
-        host: str,
-        use_session: bool,
-    ) -> None:
-        """Initialize SmartSleep client."""
-        self.somneo = Somneo(host, use_session=use_session)
-        self.state_lock = asyncio.Lock()
-
-        super().__init__(
-            hass,
-            _LOGGER,
-            name=DOMAIN,
-            update_interval=SCAN_INTERVAL,
-            update_method=self._async_update,
-            request_refresh_debouncer=Debouncer(
-                hass, _LOGGER, cooldown=1.0, immediate=False
-            ),
+async def async_setup(hass, config):
+    """Set up the SmartSleep component from yaml."""
+    if not hass.config_entries.async_entries(DOMAIN) and config.get(DOMAIN, {}):
+        # No config entry exists and configuration.yaml config exists, trigger the import flow.
+        host = config[DOMAIN][CONF_HOST]
+        name = config[DOMAIN][CONF_NAME]
+        hass.async_create_task(
+            hass.config_entries.flow.async_init(
+                DOMAIN, context={"source": SOURCE_IMPORT}, data={CONF_HOST: host, CONF_NAME: name}
+            )
         )
 
-    async def _async_update(self):
-        """Fetch the latest data."""
-        if self.state_lock.locked():
-            return
+    return True
 
-        return await self.hass.async_add_executor_job(self.somneo.fetch_data)
 
-    async def async_turn_on_light(self, brightness) -> None:
-        """Turn the device on."""
-        async with self.state_lock:
-            await self.hass.async_add_executor_job(
-                self.somneo.toggle_light, True, brightness
-            )
-            await self.async_request_refresh()
+async def async_setup_entry(hass, config_entry):
+    """Setup the SmartSleep component."""
+    try:
+        _LOGGER.info("SmartSleep setting up...")
+        _LOGGER.info("SmartSleep domain: {}".format(str(DOMAIN)))
+        hass.data[DOMAIN] = SmartSleepData(hass, config_entry)
+        await hass.data[DOMAIN].get_device_info()
+        await hass.data[DOMAIN].update()
 
-    async def async_turn_off_light(self) -> None:
-        """Turn the device on."""
-        async with self.state_lock:
-            await self.hass.async_add_executor_job(self.somneo.toggle_light, False)
-            await self.async_request_refresh()
-
-    async def async_turn_on_nightlight(self) -> None:
-        """Turn the device on."""
-        async with self.state_lock:
-            await self.hass.async_add_executor_job(self.somneo.toggle_night_light, True)
-            await self.async_request_refresh()
-
-    async def async_turn_off_nightlight(self) -> None:
-        """Turn the device on."""
-        async with self.state_lock:
-            await self.hass.async_add_executor_job(
-                self.somneo.toggle_night_light, False
-            )
-            await self.async_request_refresh()
-
-    async def async_toggle_alarm(self, alarm: str, state: bool) -> None:
-        """Toggle alarm."""
-        async with self.state_lock:
-            await self.hass.async_add_executor_job(
-                self.somneo.toggle_alarm, alarm, state
-            )
-            await self.async_request_refresh()
-
-    def set_powerwake(self, alarm: str, state: bool, delta: int):
-        """Toggle powerwake (default 10 minutes)"""
-        self.somneo.set_powerwake(alarm, onoff=state, delta=delta)
-
-    async def async_toggle_powerwake(self, alarm: str, state: bool):
-        """Toggle powerwake (default 10 minutes)"""
-        async with self.state_lock:
-            await self.hass.async_add_executor_job(self.set_powerwake, alarm, state, 10)
-            await self.async_request_refresh()
-
-    def set_alarm(self, alarm: str, hour: int | None, minute: int | None) -> None:
-        """Set alarm time."""
-        self.somneo.set_alarm(alarm, hour=hour, minute=minute)
-
-    async def async_set_alarm(
-        self, alarm: str, hours: int | None = None, minutes: int | None = None
-    ):
-        """Set alarm time."""
-        async with self.state_lock:
-            await self.hass.async_add_executor_job(
-                self.set_alarm, alarm, hours, minutes
-            )
-            await self.async_request_refresh()
-
-    async def async_set_powerwake(self, alarm: str, delta: int = 0):
-        """Set powerwake time"""
-        async with self.state_lock:
-            await self.hass.async_add_executor_job(
-                self.set_powerwake, alarm, bool(delta), delta
-            )
-            await self.async_request_refresh()
-
-    async def async_set_snooze_time(self, time):
-        """Set snooze time."""
-        async with self.state_lock:
-            await self.hass.async_add_executor_job(
-                self.somneo.set_snooze_time, int(time)
-            )
-            await self.async_request_refresh()
-
-    async def async_set_alarm_day(self, alarm, day):
-        """Set the day of the alarm."""
-        async with self.state_lock:
-            if day == WORKDAYS:
-                await self.hass.async_add_executor_job(
-                    self.somneo.set_alarm_workdays, alarm
-                )
-                _LOGGER.debug("Optie is werkday")
-            elif day == WEEKEND:
-                await self.hass.async_add_executor_job(
-                    self.somneo.set_alarm_weekend, alarm
-                )
-                _LOGGER.debug("Optie is weekend")
-            elif day == TOMORROW:
-                await self.hass.async_add_executor_job(
-                    self.somneo.set_alarm_tomorrow, alarm
-                )
-                _LOGGER.debug("Optie is morgen")
-            elif day == EVERYDAY:
-                await self.hass.async_add_executor_job(
-                    self.somneo.set_alarm_everyday, alarm
-                )
-                _LOGGER.debug("Optie is elke dag")
-
-            await self.async_request_refresh()
-
-    def set_light_alarm(
-        self, alarm: str, curve: str, level: int, duration: int
-    ) -> None:
-        """Adjust the light settings of an alarm."""
-        self.somneo.set_light_alarm(alarm, curve=curve, level=level, duration=duration)
-
-    async def async_set_light_alarm(
-        self, alarm: str, curve: str = "sunny day", level: int = 20, duration: int = 30
-    ):
-        """Adjust the light settings of an alarm."""
-        async with self.state_lock:
-            await self.hass.async_add_executor_job(
-                self.set_light_alarm, alarm, curve, level, duration
+        for platform in PLATFORMS:
+            hass.async_create_task(
+                hass.config_entries.async_forward_entry_setup(
+                    config_entry, platform)
             )
 
-    def set_sound_alarm(self, alarm: str, source: str, level: int, channel: str):
-        """Adjust the sound settings of an alarm."""
-        self.somneo.set_sound_alarm(alarm, source=source, level=level, channel=channel)
+        #### NOTHING BELOW THIS LINE ####
+        # If Success:
+        _LOGGER.info("SmartSleep has been set up!")
+        return True
+    except requests.RequestException as ex:
+        _LOGGER.error(
+            'Error while initializing SmartSleep, exception: {}'.format(str(ex)))
+        raise PlatformNotReady
+    except Exception as ex:
+        _LOGGER.error(
+            'Error while initializing SmartSleep, exception: {}'.format(str(ex)))
+        hass.components.persistent_notification.create(
+            f'Error: {str(ex)}<br />Fix issue and restart',
+            title=NOTIFICATION_TITLE,
+            notification_id=NOTIFICATION_ID)
+        # If Fail:
+        return False
 
-    async def async_set_sound_alarm(
-        self, alarm: str, source="wake-up", level=12, channel="forest birds"
-    ):
-        """Adjust the sound settings of an alarm."""
-        async with self.state_lock:
-            await self.hass.async_add_executor_job(
-                self.set_sound_alarm, alarm, source, level, channel
-            )
 
-    async def async_remove_alarm(self, alarm: str):
-        """Function to remove alarm from list in wake-up app"""
-        async with self.state_lock:
-            await self.hass.async_add_executor_job(self.somneo.remove_alarm, alarm)
+async def async_unload_entry(hass, config_entry):
+    """Uload the config entry and platforms."""
+    hass.data.pop[DOMAIN]
 
-    async def async_add_alarm(self, alarm: str):
-        """Function to add alarm to list in wake-up app"""
-        async with self.state_lock:
-            await self.hass.async_add_executor_job(self.somneo.add_alarm, alarm)
+    tasks = []
+    for platform in PLATFORMS:
+        tasks.append(hass.config_entries.async_forward_entry_unload(
+            config_entry, platform))
+
+    return all(await asyncio.gather(*tasks))
+
+
+class SmartSleepData:
+    """Handle for getting latest data from SmartSleep."""
+
+    def __init__(self, hass, config_entry):
+        """Initialize."""
+        self._hass = hass
+        self._config_entry = config_entry
+        self.somneo = Somneo(config_entry.data[CONF_HOST])
+        self.dev_info = None
+
+    async def get_device_info(self):
+        """Get device information."""
+        self.dev_info = await self._hass.async_add_executor_job(self.somneo.get_device_info)
+
+    @Throttle(UPDATE_TIME)
+    async def update(self):
+        """Get the latest update."""
+        await self._hass.async_add_executor_job(self.somneo.update)
--- custom_components/smartsleep/number.py
+++ custom_components/smartsleep/number.py
@@ -7,7 +7,7 @@ from homeassistant.const import CONF_NAME
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from .const import DOMAIN, PW_ICON, SNOOZE_ICON
+from .const import DOMAIN, HOURS, MINUTES, HOURS_ICON, MINUTES_ICON, PW_DELTA
 from .entity import SomneoEntity
 
 _LOGGER = logging.getLogger(__name__)
@@ -27,9 +27,17 @@ async def async_setup_entry(
     device_info = config_entry.data["dev_info"]
 
     alarms = []
-    # Add all PowerWake deltas
+    # Add hour & min number_entity for each alarms
     for alarm in list(coordinator.data["alarms"]):
-        alarms.append(SomneoPowerWake(coordinator, unique_id, name, device_info, alarm))
+        alarms.append(
+            SomneoTime(coordinator, unique_id, name, device_info, alarm, HOURS)
+        )
+        alarms.append(
+            SomneoTime(coordinator, unique_id, name, device_info, alarm, MINUTES)
+        )
+        alarms.append(
+            SomneoTime(coordinator, unique_id, name, device_info, alarm, PW_DELTA)
+        )
 
     snooze = [SomneoSnooze(coordinator, unique_id, name, device_info, "snooze")]
 
@@ -37,35 +45,56 @@ async def async_setup_entry(
     async_add_entities(snooze, update_before_add=True)
 
 
-class SomneoPowerWake(SomneoEntity, NumberEntity):
+class SomneoTime(SomneoEntity, NumberEntity):
     _attr_should_poll = True
     _attr_assumed_state = False
     _attr_available = True
     _attr_native_step = 1
     _attr_has_entity_name = True
-    _attr_native_min_value = 0
-    _attr_native_max_value = 59
-    _attr_icon = PW_ICON
 
-    def __init__(self, coordinator, unique_id, name, dev_info, alarm):
+    def __init__(self, coordinator, unique_id, name, dev_info, alarm, type):
         """Initialize number entities."""
-        super().__init__(
-            coordinator, unique_id, name, dev_info, alarm + "_powerwake_delta"
-        )
+        super().__init__(coordinator, unique_id, name, dev_info, alarm + "_" + type)
 
-        self._attr_translation_key = alarm + "_powerwake_delta"
+        self._attr_translation_key = alarm + "_" + type
+        if type == HOURS:
+            self._attr_native_min_value = 0
+            self._attr_native_max_value = 23
+            self._attr_icon = HOURS_ICON
+        elif type == MINUTES:
+            self._attr_native_min_value = 0
+            self._attr_native_max_value = 59
+            self._attr_icon = MINUTES_ICON
+        elif type == PW_DELTA:
+            self._attr_native_min_value = 0
+            self._attr_native_max_value = 59
+            self._attr_icon = MINUTES_ICON
 
         self._alarm = alarm
+        self._type = type
 
     @callback
     def _handle_coordinator_update(self) -> None:
-        """Handle update"""
-        self._attr_native_value = self.coordinator.data["powerwake_delta"][self._alarm]
+        if self._type == MINUTES:
+            self._attr_native_value = self.coordinator.data["alarms_minute"][
+                self._alarm
+            ]
+        elif self._type == HOURS:
+            self._attr_native_value = self.coordinator.data["alarms_hour"][self._alarm]
+        elif self._type == PW_DELTA:
+            self._attr_native_value = self.coordinator.data["powerwake_delta"][
+                self._alarm
+            ]
         self.async_write_ha_state()
 
     async def async_set_native_value(self, value: float) -> None:
         """Called when user adjust Hours / Minutes in the UI"""
-        await self.coordinator.async_set_powerwake(self._alarm, delta=int(value))
+        if self._type == MINUTES:
+            await self.coordinator.async_set_alarm(self._alarm, minutes=int(value))
+        elif self._type == HOURS:
+            await self.coordinator.async_set_alarm(self._alarm, hours=int(value))
+        elif self._type == PW_DELTA:
+            await self.coordinator.async_set_powerwake(self._alarm, delta=int(value))
 
 
 class SomneoSnooze(SomneoEntity, NumberEntity):
@@ -76,7 +105,7 @@ class SomneoSnooze(SomneoEntity, NumberEntity):
     _attr_native_min_value = 1
     _attr_native_max_value = 20
     _attr_native_step = 1
-    _attr_icon = SNOOZE_ICON
+    _attr_icon = "hass:alarm-snooze"
     _attr_has_entity_name = True
 
     @callback
